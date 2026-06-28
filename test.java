14a017c05e672950625514a9233cc0d767ca9d98
Traore <mamad.yutho@gmail.com>
2026-06-28 10:27:19 +0000

    premier commit

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int i = 9, x = 10;
+        if (i > x) {
+            System.out.println("i est plus grand que x");
+        } else if (i < x) {
+            System.out.println("i est plus petit que x");
+        } else {
+            System.out.println("i est egale a x");
+        }
+    }
+}
